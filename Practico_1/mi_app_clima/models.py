92062ef50655071e9bff7e79ca1ed02e5976275d
IvanDanielEscobar <i.escobar@itecriocuarto.org.ar>
2025-06-11 05:13:09 -0300

    integrando mysql a app.py

--- Practico_1/mi_app_clima/models.py
+++ Practico_1/mi_app_clima/models.py
@@ -5,3 +5,18 @@ class City(db.Model):
     name = db.Column(db.String(50), nullable=False, unique=True)
     lat = db.Column(db.Float, nullable=False)
     lon = db.Column(db.Float, nullable=False)
+
+#1- obtener todas las ciudades
+City.query.all()
+
+#2- obtener una ciudad por id
+City.query.get(1)
+
+#3- filtrar por nombre (exacto)
+City.query.filter_by(nombre = "cordoba").first()
+
+#4- filtrar condiciones avanzadas (OR, AND)
+from sqlalchemy import or_, and_
+City.query.filter(or_(City.lat < 0, City.lon > 100)).all()
+
+#5- Buscar por nombre parcial (como un LIKE)
